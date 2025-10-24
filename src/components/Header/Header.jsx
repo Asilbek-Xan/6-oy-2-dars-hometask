e751edc7119a2b52563aae188469717248b5b719
asilbek20000012 <asilbekshakarboev@gmail.com>
2025-10-24 15:44:04 +0500

    yangi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,40 @@
-import style from './Header.module.css';
+import { useState } from "react";
+import style from "./Header.module.css";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
-
     <header className={style.header}>
-
-
       <div className={style.left}>
         <img src="./Shortly.svg" alt="logo" className={style["header-logo"]} />
 
-        <a href="#">Features</a>
-        <a href="#">Pricing</a>
-        <a href="#">Resources</a>
+        <nav className={`${style.navLinks} ${open ? style.open : ""}`}>
+          <a href="#">Features</a>
+          <a href="#">Pricing</a>
+          <a href="#">Resources</a>
+          <div className={style.mobileAuth}>
+            <a href="#">Login</a>
+            <a href="#" className={style["header-a-button"]}>
+              Sign Up
+            </a>
+          </div>
+        </nav>
       </div>
 
       <div className={style.right}>
         <a href="#">Login</a>
-        <a href="#" className={style["header-a-button"]}>Sign Up</a>
+        <a href="#" className={style["header-a-button"]}>
+          Sign Up
+        </a>
       </div>
+
+      <img
+        src="./burger.svg"
+        alt="menu"
+        className={style.burger}
+        onClick={() => setOpen(!open)}
+      />
     </header>
   );
 }
